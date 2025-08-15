113abc058daa3b04ada82f26cbe4ebe613779daf
buroz <hey@buroz.dev>
2025-08-15 18:29:00 +0300

    lts

--- value_listener/src/main.rs
+++ value_listener/src/main.rs
@@ -1,10 +1,10 @@
-use alloy::{ 
-    consensus::Transaction, 
-    providers::{Provider, 
-    ProviderBuilder, WsConnect},
-};
+use std::error::Error;
 use futures_util::StreamExt;
-use std::{error::Error};
+use alloy::{
+    consensus::Transaction, 
+    providers::{Provider, ProviderBuilder, WsConnect}, 
+    rpc::types::eth::BlockId
+};
 
 const RPC_URL: &str = "wss://mainnet.gateway.tenderly.co";
 
@@ -17,17 +17,38 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let sub = provider.subscribe_blocks().await?;
     let mut stream = sub.into_stream();
 
-    while let Some(block_hash) = stream.next().await {
-        println!("{:?}", block_hash.hash);
-
+    while let Some(block_info) = stream.next().await {
         let provider_clone = provider.clone();
-        
+
         tokio::spawn(async move {
-            let block = provider_clone.get_block_by_hash(block_hash.hash).await.unwrap().unwrap();
-            let transactions = block.transactions.as_transactions().unwrap();
-            for (_, tx) in transactions.iter().enumerate() {
-                let value = tx.value();
-                println!("{}", value);
+            let block_id = BlockId::from(block_info.hash);
+            match provider_clone.get_block(block_id).await {
+                Ok(Some(block)) => {
+                    let transactions = block.transactions.as_hashes().unwrap();
+                    
+                    for tx_hash in transactions {
+                        let tx = provider_clone.get_transaction_by_hash(*tx_hash).await.unwrap().unwrap();
+                        let value = tx.value();
+                        if value > 0 {
+                            let tx_request = tx.into_request();
+                            let from = tx_request.from.unwrap();
+                            let to = tx_request.to.unwrap().into_to().unwrap();
+                            println!(
+                                "{} | {}->{} : {:?} wei", 
+                                tx_hash.to_string(), 
+                                from.to_string().chars().take(5).collect::<String>(), 
+                                to.to_string().chars().take(5).collect::<String>(), 
+                                value
+                            );
+                        }
+                    }
+                }
+                Ok(None) => {
+                    eprintln!("Block not found: {:?}", block_info.hash);
+                }
+                Err(e) => {
+                    eprintln!("Error fetching block: {:?}", e);
+                }
             }
         });
     }
