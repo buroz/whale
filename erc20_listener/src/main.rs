4faaa91aa46770218128b449826a8700daaff4ae
buroz <hey@buroz.dev>
2025-08-16 18:56:55 +0300

    monitoring

--- erc20_listener/src/main.rs
+++ erc20_listener/src/main.rs
@@ -1,19 +1,12 @@
 use alloy::{
-    providers::{Provider, ProviderBuilder, WsConnect}, 
-    rpc::types::{BlockNumberOrTag, Filter}, 
-    sol, 
-    sol_types::SolEvent
-};
-use futures_util::StreamExt;
-use questdb::{
-    ingress::{
-        Sender,
-        Buffer,
-        TimestampMicros,
-        ProtocolVersion
-    },
+    providers::{Provider, ProviderBuilder, WsConnect},
+    rpc::types::{BlockNumberOrTag, Filter},
+    sol,
+    sol_types::SolEvent,
 };
 use anyhow::Result;
+use futures_util::StreamExt;
+use questdb::ingress::{Buffer, ProtocolVersion, Sender, TimestampMicros};
 use std::sync::{Arc, Mutex};
 
 const RPC_URL: &str = "wss://mainnet.gateway.tenderly.co";
@@ -28,12 +21,14 @@ sol! {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let sender = Sender::from_conf("http::addr=localhost:9000;username=admin;password=quest;retry_timeout=20000;")
-        .map_err(|e| anyhow::anyhow!("QuestDB sender error: {}", e))?;
-    
+    let sender = Sender::from_conf(
+        "http::addr=questdb:9000;username=admin;password=quest;retry_timeout=20000;",
+    )
+    .map_err(|e| anyhow::anyhow!("QuestDB sender error: {}", e))?;
+
     let sender = Arc::new(Mutex::new(sender));
 
-    let ws = WsConnect::new(RPC_URL); 
+    let ws = WsConnect::new(RPC_URL);
     let provider = ProviderBuilder::new().connect_ws(ws).await?;
 
     let filter = Filter::new()
@@ -42,7 +37,7 @@ async fn main() -> Result<()> {
 
     let sub = provider.subscribe_logs(&filter).await?;
     let mut stream = sub.into_stream();
-  
+
     while let Some(log) = stream.next().await {
         let sender_clone = sender.clone();
 
@@ -56,7 +51,10 @@ async fn main() -> Result<()> {
 
                 if amount == "0" {
                     buffer.clear();
-                    println!("Amount is 0 skipping: {:?}", log.transaction_hash.unwrap().to_string());
+                    println!(
+                        "Amount is 0 skipping: {:?}",
+                        log.transaction_hash.unwrap().to_string()
+                    );
                     return;
                 }
 
@@ -65,7 +63,12 @@ async fn main() -> Result<()> {
                     .and_then(|b| b.column_str("token_address", address.to_string()))
                     .and_then(|b| b.column_str("from", transfer.from.to_string()))
                     .and_then(|b| b.column_str("to", transfer.to.to_string()))
-                    .and_then(|b| b.column_str("transaction_hash", log.transaction_hash.unwrap().to_string()))
+                    .and_then(|b| {
+                        b.column_str(
+                            "transaction_hash",
+                            log.transaction_hash.unwrap().to_string(),
+                        )
+                    })
                     .and_then(|b| b.column_i64("log_index", log.log_index.unwrap() as i64))
                     .and_then(|b| b.column_i64("chain_id", 1))
                     .and_then(|b| b.column_i64("block_number", log.block_number.unwrap() as i64))
